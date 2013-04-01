fc27aa5820f80d7c2331ab6caaec532a07f6ae32
gobwas <gobwas@gmail.com>
2013-04-01 23:42:26 +0400

    =)

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             options: {
-                separator: ';\n\n\n',
+                separator: '\n\n\n',
                 banner: grunt.file.read('build/banner.txt')
             },
             dist: {
